1776b0b28b53736b449679ccaada07b0e6e58817
Simon Frankau <sgf@arbitrary.name>
2014-08-12 22:38:57 +0100

    Working solution(?)

--- src/name/arbitrary/Main.java
+++ src/name/arbitrary/Main.java
@@ -1,7 +1,11 @@
 package name.arbitrary;
 
-import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -9,17 +13,41 @@ public class Main {
         BufferedReader input = getInput(args[0]);
         int numLines = Integer.parseInt(getString(input));
         for (int i = 0; i < numLines; ++i) {
-            System.out.println(runCase(getString(input)));
+            System.out.println("Case #" + (i+1) + ": " + runCase(getString(input)));
         }
     }
 
     private static String runCase(String string) {
         String parts[] = string.split(" ");
-        StringBuffer sb = new StringBuffer();
-        for (String part : parts) {
-            sb.append(part).append("-");
+        String numStr = parts[0];
+        String source = parts[1];
+        String target = parts[2];
+
+        Map<Character, Integer> srcMap = new HashMap<Character, Integer>();
+        int i = 0;
+        for (char c : source.toCharArray()) {
+            srcMap.put(c, i++);
         }
-        return sb.toString();
+
+        long num = 0;
+        for (char c : numStr.toCharArray()) {
+            num *= source.length();
+            num += srcMap.get(c);
+        }
+
+        char[] tgtMap = target.toCharArray();
+
+        if (num == 0) {
+            return "" + tgtMap[0];
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (num > 0) {
+            result.append(tgtMap[(int) (num % target.length())]);
+            num /= target.length();
+        }
+
+        return result.reverse().toString();
     }
 
     private static String getString(BufferedReader input) {
