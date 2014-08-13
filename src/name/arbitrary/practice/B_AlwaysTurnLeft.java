6c4fe6ed29ad97fa4a0f8479ffcfad79ad8b46a9
Simon Frankau <sgf@arbitrary.name>
2014-08-13 20:46:21 +0100

    Main implementation of Always Turn Left

--- src/name/arbitrary/practice/B_AlwaysTurnLeft.java
+++ src/name/arbitrary/practice/B_AlwaysTurnLeft.java
@@ -1,5 +1,7 @@
 package name.arbitrary.practice;
 
+import com.sun.tools.javac.util.Pair;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -24,6 +26,10 @@ public class B_AlwaysTurnLeft {
     int x = 0;
     int y = 0;
 
+    Facing endDir;
+
+    Map<Pair<Integer, Integer>, Integer> wallMap = new HashMap<Pair<Integer, Integer>, Integer>();
+
     int minX = Integer.MAX_VALUE;
     int minY = Integer.MAX_VALUE;
     int maxX = Integer.MIN_VALUE;
@@ -31,26 +37,50 @@ public class B_AlwaysTurnLeft {
 
     private String runCase(String string) {
         String parts[] = string.split(" ");
-        String entrance_to_exit = parts[0];
-        String exit_to_entrance = parts[1];
+        String entrance_to_exit = rejig(parts[0]);
+        String exit_to_entrance = rejig(parts[1]);
 
         followPath(entrance_to_exit);
+        endDir = dir;
         turnLeft(); turnLeft();
         followPath(exit_to_entrance);
+        adjustEdges();
 
-        return "FIXME";
+        return map();
+    }
+
+    private void adjustEdges() {
+        minY++;
+        switch (endDir) {
+            case N:         break;
+            case S: maxY--; break;
+            case E: maxX--; break;
+            case W: minX++; break;
+        }
+    }
+
+    // The idea of 'rejig' is to make each step into an explicit 'find leftmost path' step - at each step, we turn
+    // left then turn right until the way is clear, so that each right turn will now represent a wall.
+    private String rejig(String path) {
+        // NB: Replace order is important...
+        path = path.replaceAll("LW", "lw");
+        path = path.replaceAll("RRW", "lrrrw");
+        path = path.replaceAll("RW", "lrrw");
+        path = path.replaceAll("W", "lrw");
+        return path;
     }
 
     private void followPath(String path) {
         for (char c : path.toCharArray()) {
             switch (c) {
-                case 'L':
+                case 'l':
                     turnLeft();
                     break;
-                case 'R':
+                case 'r':
+                    markWall();
                     turnLeft(); turnLeft(); turnLeft();
                     break;
-                case 'W':
+                case 'w':
                     forward();
                     break;
                 default:
@@ -61,8 +91,6 @@ public class B_AlwaysTurnLeft {
             minY = Math.min(minY, y);
             maxX = Math.max(maxX, x);
             maxY = Math.max(maxY, y);
-
-            System.out.println(x + ", " + y);
         }
     }
 
@@ -82,6 +110,42 @@ public class B_AlwaysTurnLeft {
             case S: dir = Facing.E; break;
             case W: dir = Facing.S; break;
         }
+    }
+
+    private void markWall() {
+        Pair coord = Pair.of(x,y);
+        Integer wallVal = wallMap.get(coord);
+        if (wallVal == null) {
+            wallVal = 0;
+        }
+        switch (dir) {
+            case N: wallVal |= 1; break;
+            case S: wallVal |= 2; break;
+            case W: wallVal |= 4; break;
+            case E: wallVal |= 8; break;
+        }
+        wallMap.put(coord, wallVal);
+    }
+
+    private static final char[] chars = "0123456789abcdef".toCharArray();
+
+    private char wallAt(int x, int y) {
+        Integer wallVal = wallMap.get(Pair.of(x, y));
+        if (wallVal == null) {
+            wallVal = 0;
+        }
+        return chars[wallVal ^ 0xf];
+    }
+
+    private String map() {
+        StringBuffer sb = new StringBuffer();
+        for (int y = minY; y <= maxY; y++) {
+            sb.append('\n');
+            for (int x = minX; x <= maxX; x++) {
+                sb.append(wallAt(x, y));
+            }
+        }
+        return sb.toString();
     }
 
     private static String getString(BufferedReader input) {
