f302e67ad1c8c84115ad5712657e59145d4c1577
Simon Frankau <sgf@arbitrary.name>
2014-08-13 23:21:15 +0100

    Fixed EggDrop algorithm for large data set

--- src/name/arbitrary/practice/C_EggDrop.java
+++ src/name/arbitrary/practice/C_EggDrop.java
@@ -14,8 +14,9 @@ public class C_EggDrop {
     public static void main(String[] args) {
         BufferedReader input = getInput(args[0]);
         int numLines = Integer.parseInt(getString(input));
+        C_EggDrop run = new C_EggDrop();
+        run.fillAll();
         for (int i = 0; i < numLines; ++i) {
-            C_EggDrop run = new C_EggDrop();
             System.out.println("Case #" + (i+1) + ": " + run.runCase(getString(input)));
         }
     }
@@ -27,47 +28,40 @@ public class C_EggDrop {
     }
 
     private long get(int drops, int breaks) {
-        Long res = cache.get(Pair.of(drops, breaks));
-        if (res != null) {
-            return res;
-        }
-        return calcAndCache(drops, breaks);
-    }
-
-    private long calcAndCache(int drops, int breaks) {
-        System.out.println(drops + " " + breaks); // FIXME
-        long floors = calc(drops, breaks);
-        add(drops, breaks, floors);
-        System.out.println(drops + " " + breaks + " -> " + floors); // FIXME
-        return floors;
-    }
-
-    private long calc(int drops, int breaks) {
-        if (drops <= breaks) {
-            // Limiting factor is drops, can binary chop
-            return (1 << drops) - 1;
-        }
-        if (breaks >= 32) {
-            // >= 32 drops and breaks means can do binary chop on full 32-bit space.
-            return -1;
-        }
+        // First deal with corner cases we don't want to cache...
         if (breaks == 1) {
             // Linear scan
             return drops;
         }
-        long caseA = get(drops - 1, breaks - 1);
-        if (caseA < 0) {
-            return -1;
+        if (drops <= breaks) {
+            // Limited by drops, binary chop
+            return (1L << drops) - 1;
         }
-        long caseB = get(drops - 1, breaks);
-        if (caseB < 0) {
-            return -1;
+        Long res = cache.get(Pair.of(drops, breaks));
+        if (res != null) {
+            return res;
         }
-        long res = caseA + caseB + 1;
-        if (res >= 1L << 32) {
-            return -1;
+        return -1;
+    }
+
+    private void fillLevel(int breaks) {
+        // Skip the part where drops <= breaks, which we don't cache.
+        int drops = breaks + 1;
+        for (;;) {
+            long floors = 1 + get(drops - 1, breaks) + get(drops - 1, breaks - 1);
+            if (floors >= (1L << 32)) {
+                break;
+            }
+            add(drops, breaks, floors);
+            drops++;
         }
-        return res;
+    }
+
+    private void fillAll() {
+        // 33 breaks, with binary chop, gets up to 2^32.
+        for (int breaks = 2; breaks <= 33; breaks++) {
+            fillLevel(breaks);
+        }
     }
 
     private String runCase(String string) {
@@ -76,20 +70,16 @@ public class C_EggDrop {
         int drops  = Integer.parseInt(parts[1]);
         int breaks = Integer.parseInt(parts[2]);
 
-        System.out.println("A");
-        long maxFloors = calc(drops, breaks);
-        System.out.println("B");
+        long maxFloors = get(drops, breaks);
         int minDrops   = findMinDrops(floors, breaks);
-        System.out.println("C");
         int minBreaks  = findMinBreaks(floors, drops);
-        System.out.println("D");
 
         return maxFloors + " " + minDrops + " " + minBreaks;
     }
 
     private int findMinDrops(long floors, int breaks) {
         int drops = 1;
-        while (get(drops, breaks) < floors) {
+        while (get(drops, breaks) < floors && get(drops, breaks) != -1) {
             drops++;
         }
         return drops;
@@ -97,7 +87,7 @@ public class C_EggDrop {
 
     private int findMinBreaks(long floors, int drops) {
         int breaks = 1;
-        while (get(drops, breaks) < floors) {
+        while (get(drops, breaks) < floors && get(drops, breaks) != -1) {
             breaks++;
         }
         return breaks;
