a310674adbbcd2e9753285423a927ceda035880f
Simon Frankau <sgf@arbitrary.name>
2014-08-16 11:22:12 +0100

    Added deceitful war case, works on example cases...

--- src/name/arbitrary/y2014/qualification/D_DeceitfulWar.java
+++ src/name/arbitrary/y2014/qualification/D_DeceitfulWar.java
@@ -20,8 +20,8 @@ public class D_DeceitfulWar extends CodeJamBase {
         SortedSet<Double> kenBlocks = getBlocks(nItems);
 
         int warScore = playWar(naomiBlocks, kenBlocks);
-
-        return "? " + warScore;
+        int deceitfulWarScore = playDeceitfulWar(naomiBlocks, kenBlocks);
+        return deceitfulWarScore + " " + warScore;
     }
 
     private SortedSet<Double> getBlocks(int nItems) {
@@ -49,6 +49,30 @@ public class D_DeceitfulWar extends CodeJamBase {
             } else {
                 // Ken can win using the smallest one that is larger.
                 kens.remove(largerBlocks.first());
+            }
+        }
+        return score;
+    }
+
+    private int playDeceitfulWar(SortedSet<Double> naomiBlocks, SortedSet<Double> kenBlocks) {
+        // If Ken's largest block beats our largest, it will win its round, so eliminate it using our smallest.
+        // Do this whenever our largest is not larger than his largest. When ours is largest, win a round!
+
+        // Going to mutate them.
+        TreeSet<Double> kens   = new TreeSet<Double>(kenBlocks);
+        TreeSet<Double> naomis = new TreeSet<Double>(naomiBlocks);
+        int score = 0;
+        // Want to iterate backwards, can't be bothered to change comparator, reverse sign, etc.
+        while (!kens.isEmpty()) {
+            if (kens.last() > naomis.last()) {
+                // Sacrifice smallest with a lie.
+                kens.remove(kens.last());
+                naomis.remove(naomis.first());
+            } else {
+                // Win!
+                kens.remove(kens.last());
+                naomis.remove(naomis.last());
+                score++;
             }
         }
         return score;
